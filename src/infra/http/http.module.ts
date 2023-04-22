efcfe28dfb0592ba48885ab68e778d5cc42788f8
Luan-max <luannlopessiva@outlook.com>
2023-04-22 11:33:05 -0300

    feat: add use case in http module provider

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { UsersController } from './controllers/users.controller';
 import { CreateUser } from 'src/application/use-cases/create-user';
+import { FindAllUsers } from 'src/application/use-cases/find-all-users';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [CreateUser],
+  providers: [CreateUser, FindAllUsers],
 })
 export class HttpModule {}
