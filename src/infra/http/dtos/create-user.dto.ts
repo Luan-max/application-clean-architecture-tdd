1c13dc9aef9c816b127e349b83651b65003e6300
Luan-max <luannlopessiva@outlook.com>
2023-04-22 11:44:09 -0300

    feat: add treatment for docs

--- src/infra/http/dtos/create-user.dto.ts
+++ src/infra/http/dtos/create-user.dto.ts
@@ -1,5 +1,24 @@
+import { IsString, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDTO {
+  @IsString()
+  @ApiProperty({
+    description: 'Name',
+    example: 'John',
+  })
   name: string;
+  @IsString()
+  @ApiProperty({
+    description: 'Document',
+    example: '999999999999',
+  })
   document: string;
+  @IsString()
+  @IsEmail()
+  @ApiProperty({
+    description: 'E-mail',
+    example: 'johndoe@jhon.com',
+  })
   email: string;
 }
