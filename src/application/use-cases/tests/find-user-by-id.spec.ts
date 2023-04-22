4619c9ca95f8752a42e570736b9f892c673a2dca
Luan-max <luannlopessiva@outlook.com>
2023-04-22 13:28:06 -0300

    feat: add testes for find one use case

---
+++ src/application/use-cases/tests/find-user-by-id.spec.ts
@@ -0,0 +1,19 @@
+import { InMemoryUsersRepository } from '../../repositories/users-in-memory-repository';
+import { FindUserById } from '../find-user-by-id';
+import { makeUser } from './factories/user.factory';
+
+describe('FindUserById - useCase', () => {
+  it('when find one user', async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const findOneUserUseCase = new FindUserById(usersRepository);
+
+    const createUser = makeUser();
+
+    await usersRepository.create(createUser);
+
+    const { user } = await findOneUserUseCase.execute({
+      userId: createUser.id,
+    });
+    expect(user).toEqual(createUser);
+  });
+});
