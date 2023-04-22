664560fe3c498af6c3c05e59d0af76c8f46c243b
Luan-max <luannlopessiva@outlook.com>
2023-04-22 11:33:23 -0300

    feat: add endpoint in my controller for list

--- src/infra/http/controllers/users.controller.ts
+++ src/infra/http/controllers/users.controller.ts
@@ -1,11 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateUserDTO } from '../dtos/create-user.dto';
 import { CreateUser } from '../../../application/use-cases/create-user';
+import { FindAllUsers } from '../../../application/use-cases/find-all-users';
 import { UserViewModel } from '../view-models/user.view-model';
+import { User } from 'src/application/entities/user';
 
 @Controller('users')
 export class UsersController {
-  constructor(private createUser: CreateUser) {}
+  constructor(
+    private createUser: CreateUser,
+    private findAllUsers: FindAllUsers,
+  ) {}
 
   @Post()
   async create(@Body() body: CreateUserDTO) {
@@ -20,5 +25,11 @@ export class UsersController {
     return {
       user: UserViewModel.toHTTP(user),
     };
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    const { users } = await this.findAllUsers.execute();
+    return users;
   }
 }
