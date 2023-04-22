56ed472ddc9e93a7dfbfce8326a47f0c5bba43e9
Luan-max <luannlopessiva@outlook.com>
2023-04-22 11:58:18 -0300

    feat: create abstract method findOne

--- src/application/repositories/user.repository.ts
+++ src/application/repositories/user.repository.ts
@@ -3,4 +3,5 @@ import { User } from '../entities/user';
 export abstract class UserRepository {
   abstract create(user: User): Promise<void>;
   abstract findAllUsers(): Promise<User[]>;
+  abstract findOneUser(userId: string): Promise<User>;
 }
