7f48809ff13a0220ec1b013fd066efa72453dc19
Luan-max <luannlopessiva@outlook.com>
2023-04-22 11:46:11 -0300

    feat: remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
